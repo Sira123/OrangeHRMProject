562247fb3051bb50bb21b9474e97378147d72b78
Sira123 <harshavardhan.sira@gmail.com>
2020-02-24 09:58:40 +0530

    first commit

---
+++ OrangeHRM/src/main/java/com/orangehrm/utilites/ValidationOperation.java
@@ -0,0 +1,20 @@
+package com.orangehrm.utilites;
+
+import org.testng.Assert;
+
+public class ValidationOperation {
+	
+	public static String validateTestScript(String exp,String act)
+	{
+	  try {
+		  Assert.assertEquals(exp,act);
+		  return "pass";
+	  }
+	  catch(AssertionError rv)
+	  {
+		  return "fail";
+	  }
+		
+	}
+
+}
