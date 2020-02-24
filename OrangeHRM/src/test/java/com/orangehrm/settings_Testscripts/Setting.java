88010474ad0ab07224795f815849ebc0d3015193
Sira123 <harshavardhan.sira@gmail.com>
2020-02-24 10:33:07 +0530

    first commit as TE

---
+++ OrangeHRM/src/test/java/com/orangehrm/settings_Testscripts/Setting.java
@@ -0,0 +1,16 @@
+package com.orangehrm.settings_Testscripts;
+
+import org.testng.annotations.Test;
+
+import com.orangehrm.utilites.BaseClass;
+
+public class Setting extends BaseClass {
+	
+	
+	@Test
+	public void testSettings()
+	{
+		//////
+	}
+
+}
